abebfc787950fbf2f952ef02c1d5851e0b79028d
Henrique <henrique.v.curti@gmail.com>
2024-11-13 22:03:21 -0300

    vite config default

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: ["chrome89", "edge89", "firefox78", "safari14"], // Ajuste conforme necessário
-  },
+  plugins: [react()],
 });
